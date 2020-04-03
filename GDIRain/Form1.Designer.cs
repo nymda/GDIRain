2ddb021f6a8c9ef67f03124d07a151325a3d9e07
jake hartley <puffleboy101@hotmail.co.uk>
2020-04-03 22:25:39 +0100

    commit

--- GDIRain/Form1.Designer.cs
+++ GDIRain/Form1.Designer.cs
@@ -29,12 +29,44 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.drawFrame = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(712, 564);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // drawFrame
+            // 
+            this.drawFrame.Interval = 5;
+            this.drawFrame.Tick += new System.EventHandler(this.drawFrame_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(712, 564);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Rain";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer drawFrame;
     }
 }
 
--- GDIRain/Form1.cs
+++ GDIRain/Form1.cs
@@ -16,5 +16,76 @@ namespace GDIRain
         {
             InitializeComponent();
         }
+
+        public Random rnd = new Random();
+        public List<droplet> drops = new List<droplet> { };
+        public Bitmap bg;
+        public Graphics g;
+        public Pen thick = new Pen(Brushes.LightBlue, 2);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initialise();
+
+            for(int i = 0; i < 50; i++)
+            {
+                spawnDrop();
+            }
+
+            drawFrame.Start();
+        }
+
+        public void initialise()
+        {
+            bg = new Bitmap(this.Width, this.Height);
+            g = Graphics.FromImage(bg);
+        }
+
+        public void spawnDrop()
+        {
+            int newY = bg.Height;
+            int newX = rnd.Next(0, bg.Width);
+            int newZ = rnd.Next(0, 10);
+            int newlen = rnd.Next(5, 15);
+            drops.Add(new droplet(newY, newX, newZ, newlen));
+        }
+
+        private void drawFrame_Tick(object sender, EventArgs e)
+        {
+            spawnDrop();
+            spawnDrop();
+            g.FillRectangle(Brushes.Gray, 0, 0, bg.Width, bg.Height);
+            for(int i = 0; i < drops.Count; i++)
+            {
+                int drawX = drops[i].x;
+                int drawY = drops[i].y;
+                int length = drops[i].length;
+                drawY = bg.Height - drawY;
+                if(drawY < bg.Height - 1)
+                {
+                    g.DrawLine(thick, drawX, drawY, drawX, drawY - length);
+                    drops[i].y -= (1 * drops[i].z) * 2;
+                }
+
+            }
+            pictureBox1.Image = bg;
+        }
+
+    }
+
+    public class droplet
+    {
+        public int y { get; set; }
+        public int x { get; set; }
+        public int z { get; set; }
+        public int length { get; set; }
+
+        public droplet(int yPos, int xPos, int zPos, int len)
+        {
+            y = yPos;
+            x = xPos;
+            z = zPos;
+            length = len;
+        }
     }
 }
